9bcc59c9d19963ec6227af85f41414f6db37c34d
Ryanpez <ryansynez@gmail.com>
2023-09-21 15:29:04 -0500

    uncommented for loops

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,23 +4,23 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
-   // for (let i = 0; i < 21; i ++) {
-  //console.log(i);
-//}
+    for (let i = 0; i < 21; i ++) {
+  console.log(i);
+}
 
-//for (let i = 3; i < 30; i += 2) {
- // console.log(i);
-//}
+for (let i = 3; i < 30; i += 2) {
+  console.log(i);
+}
 
-//for (let i = 12; i > -15; i -= 2) {
-//  console.log(i);
-//}
+for (let i = 12; i > -15; i -= 2) {
+  console.log(i);
+}
 
-//for (let i = 50; i > 19; i -= 1) {
- // if (i % 3 === 0) {
- //   console.log(i);
- // }
-//}
+for (let i = 50; i > 19; i -= 1) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
 
 
 
@@ -33,13 +33,13 @@ let arr = [1, 5, 'LC101', 'blue', 42];
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
-//for (let i = 0; i < arr.length; i++) {
- // console.log(arr[i]);
-//}
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
 
-//for (let i = 9; i > -1; i -= 1) {
- // console.log(str[i]);
-//}
+for (let i = 9; i > -1; i -= 1) {
+  console.log(str[i]);
+}
 
 
 
