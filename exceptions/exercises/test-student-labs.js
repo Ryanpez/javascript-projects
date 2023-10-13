4fae37f84129a1a37cd2374cb576e82a44fc0f7c
Ryanpez <ryansynez@gmail.com>
2023-10-12 19:38:15 -0500

    completed exceptions exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,6 +4,12 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+try {
+  console.log(studentlabs.runlab());
+  console.log(studentlabs2.runlab());
+} catch(err) {
+  console.log("Error Thrown");
+}
 }
 
 let studentLabs = [
@@ -22,3 +28,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
