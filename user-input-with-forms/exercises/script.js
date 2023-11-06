2c942ea60cb668c60a01f5daf9432a805234dd07
Ryan Gregory <ryansynez@gmail.com>
2023-11-05 22:35:18 -0600

    completed forms exercise

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,20 @@
-//Code Your Solution Below
+window.addEventListener("load", function() {
+
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let name = document.querySelector("input[name=testName]");
+        let date = document.querySelector("input[name=testDate]");
+        let rocket = document.querySelector("input[name=rocketType]");
+        let booster = document.querySelector("input[name=boosterCount]");
+        let wind = document.querySelector("input[name=windRating]");
+        let prod = document.querySelector("input[name=productionServers");
+
+        if (name.input.value === "" || date.input.value === "" || rocket.input.value === "" || 
+        booster.input.value === "" || wind.input.value === "" || prod.input.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+    });
+
+});
+
